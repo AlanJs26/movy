e99aa61f2cc87c0e28bf272826da530ab27c4367
AlanJS <alanjoses.29@gmail.com>
2023-04-03 19:00:36 -0300

    packed script as a module

--- movy/compile.bat
+++ movy/compile.bat
@@ -1,6 +1,6 @@
 pyinstaller movy_organizer.spec -y
-cp -r "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/scripts" "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/dist/movy_organizer"
-cp "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/settings.yaml" "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/dist/movy_organizer"
-cp "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/icon.png" "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/dist/movy_organizer"
-cp -r "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/pt_core_news_sm" "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/dist/movy_organizer"
-cp -r "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/pt_core_news_sm-3.4.0.dist-info" "G:/Users/Alan/Documents/_Codes/Python/Text Processing/movy/dist/movy_organizer"
+cp -r "scripts" "dist/movy_organizer"
+cp "settings.yaml" "dist/movy_organizer"
+cp "icon.png" "dist/movy_organizer"
+cp -r "pt_core_news_sm" "dist/movy_organizer"
+cp -r "pt_core_news_sm-3.4.0.dist-info" "dist/movy_organizer"
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="movy",
+    license = "MIT",
+    author="AlanJS26",
+    author_email="alanjoses.29@gmail.com",
+    description="organize all your files programmatically",
+    packages=find_packages(),
+    url="https://github.com/AlanJs26/movy",
+    include_package_data=True,
+    python_requires = ">=3.10"
+)
+
